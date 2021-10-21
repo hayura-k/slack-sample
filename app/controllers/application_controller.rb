f62af53f64da2cc401382aed066a34ab97b8d058
yuki hara <hara.0509@icloud.com>
2021-10-22 01:55:51 +0900

    slack機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  def hello
+    client = Slack::Web::Client.new
+    client.chat_postMessage(
+      channel: '#テスト',
+      text: 'こんにちは'
+    )
+  end
+
 end
